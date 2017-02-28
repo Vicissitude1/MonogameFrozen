1504036669674689aad35398f2664f0fa2b8295d
Nvhen <Nvhen@LAPTOP-VUTMMO7N>
2017-02-28 15:24:11 +0100

    Comma comma

--- Frozen/Frozen/Interface/IAnimateable.cs
+++ Frozen/Frozen/Interface/IAnimateable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-e
+
 namespace Frozen
 {
     interface IAnimateable
--- Frozen/Frozen/SpriteRenderer.cs
+++ Frozen/Frozen/SpriteRenderer.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace Frozen
 {
     class SpriteRenderer
     {
+        public Rectangle rectangle { get; set; }
+        public Texture2D sprite { get; set; }
+        private string spriteName;
+        private float layerDepth;
+        public Vector2 Offset { get; set; }
+
+        public SpriteRenderer(GameObject gameObject, string spriteName, float layerDepth) : base(gameObject)
+        {
+            this.spriteName = spriteName;
+            this.layerDepth = layerDepth;
+
+        }
+
+        public void update()
+        {
+
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            
+        }
+
     }
 }
