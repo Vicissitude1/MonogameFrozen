76e03af657d9f64b6e2c14abad0818ac77853c03
theni <theni@LAPTOP-Q9ORS9IL>
2017-02-28 15:49:53 +0100

    sd

--- Frozen/Frozen/SpriteRenderer.cs
+++ Frozen/Frozen/SpriteRenderer.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace Frozen
 {
     class SpriteRenderer
     {
+        public Rectangle rectangle { get; private set; }
+        public Texture2D sprite { get; private set; }
+        private string spriteName;
+        private float layerDepth;
+        private Vector2 offset;
+
+        public SpriteRenderer(GameObject gameObject, string spriteName, float layerDepth)
+        {
+
+        }
+        public void LoadContent()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
     }
 }
--- Frozen/Frozen/Transform.cs
+++ Frozen/Frozen/Transform.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
 namespace Frozen
 {
     class Transform
     {
+        public Vector2 Position { get; private set; }
 
+        public Transform(GameObject gameObject, Vector2 position)
+        {
+
+        }
+
+        public void Translate(Vector2 translation)
+        {
+
+        }
     }
+
 }
