1750350d5b81ffb112860ffec5af3aeabb9c2ed6
theni <theni@LAPTOP-Q9ORS9IL>
2017-03-01 10:18:47 +0100

    4

--- Frozen/Frozen/Player.cs
+++ Frozen/Frozen/Player.cs
@@ -8,9 +8,43 @@ namespace Frozen
 {
     class Player
     {
-        private float speed;
+        public float Speed { get; private set; }
         public Animator animator;
+        public bool canMove;
         private IStrategy strategy;
         private DIRECTION direction;
+
+
+        public Player()
+        {
+
+        }
+        
+        public void Execute(ref DIRECTION CurrendtDirecion)
+        {
+
+        }
+
+
+        public void Update()
+        {
+
+        }
+
+        public void CreateAnimation()
+        {
+            //animator.CreateAnimation(new Animation(4, 0, 0, 90, 150, 6, Vector2.Zero), "IdleFront");
+            //animator.PlayAnimation("IdleFront");
+        }
+
+        public void OnAnimationDone()
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
     }
 }
