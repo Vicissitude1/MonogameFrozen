f867cc5549843b7db9dc6a4e07065bae66e91591
theni <theni@LAPTOP-Q9ORS9IL>
2017-03-03 12:32:12 +0100

    44444444

--- Frozen/Frozen/Player.cs
+++ Frozen/Frozen/Player.cs
@@ -93,6 +93,7 @@ namespace Frozen
             //animator.CreateAnimation(new Animation(3, 1070, 0, 150, 150, 5, Vector2.Zero), "DieLeft");
             //animator.CreateAnimation(new Animation(3, 1070, 3, 150, 150, 5, Vector2.Zero), "DieRight");
 
+            //bitch
             animator.PlayAnimation("IdleRight");
 
             strategy = new Idle(animator);
