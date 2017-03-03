9d523a3a6bad65075c49e346969594a2c12ad89c
theni <theni@LAPTOP-Q9ORS9IL>
2017-03-03 13:34:51 +0100

    12

---
+++ Frozen/Frozen/Jump.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frozen
+{
+    class Jump : IStrategy
+    {
+        public Animator animator { get; set; }
+
+        public Jump(Animator animator)
+        {
+            this.animator = animator;
+        }
+        public void Execute(ref DIRECTION CurrentDirection)
+        {
+            animator.PlayAnimation("Jump" + CurrentDirection);
+        }
+    }
+}
+
+   
