bfb7c75ea6b7041ec4a7b5b9646b5be3b841d82a
Nvhen <Nvhen@LAPTOP-VUTMMO7N>
2017-03-06 11:21:21 +0100

    Viewport

--- Frozen/Frozen/Camera.cs
+++ Frozen/Frozen/Camera.cs
@@ -69,7 +69,7 @@ namespace Frozen
               Matrix.CreateTranslation(new Vector3(-pos.X, -pos.Y, 0)) *
                                          Matrix.CreateRotationZ(Rotation) *
                                          Matrix.CreateScale(new Vector3(Zoom, Zoom, 1)) *
-                                         Matrix.CreateTranslation(new Vector3(ViewportWidth * 0.5f, ViewportHeight * 0.5f, 0));
+                                         Matrix.CreateTranslation(new Vector3(graphicsDevice.Viewport.Width * 0.5f, graphicsDevice.Viewport.Height * 0.5f, 0));
             return transform;
         }
 
