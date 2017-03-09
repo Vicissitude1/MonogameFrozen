96bffccc0968f659fb913ff038b9a5c5a3a37429
theni <theni@LAPTOP-Q9ORS9IL>
2017-03-09 13:25:15 +0100

    45

--- Frozen/Frozen/SFX.cs
+++ Frozen/Frozen/SFX.cs
@@ -1,13 +1,62 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using System;
 
 namespace Frozen
 {
-    class SFX
+    class SFX : ILoadable
     {
+        private static SFX instance;
+        public static SFX Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SFX();
+                }
+                return instance;
+            }
+        }
 
+        public static List<SoundEffect> soundEffects;
+
+        private SFX()
+        {
+
+            soundEffects = new List<SoundEffect>();
+        }
+
+       
+        
+
+
+       
+       
+        public void LoadContent(ContentManager content)
+        {
+             
+        
+            
+
+
+            soundEffects.Add(content.Load<SoundEffect>("sound1"));
+            soundEffects.Add(content.Load<SoundEffect>("sound2"));
+          
+
+            // Fire and forget play
+            soundEffects[0].Play();
+
+            // Play that can be manipulated after the fact
+            var instance = soundEffects[0].CreateInstance();
+            instance.IsLooped = true;
+            instance.Play();
+        }
+    
     }
 }
+
